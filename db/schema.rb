ffed2eda037bb3a2dd15814d1319f21514b8589b
MokumJ <jesse.vanderheijden@gmail.com>
2017-11-21 13:22:47 +0100

    views up and working

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-
-
 ActiveRecord::Schema.define(version: 20171121105617) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,15 +22,12 @@ ActiveRecord::Schema.define(version: 20171121105617) do
     t.datetime "updated_at", null: false
   end
 
-
-
   create_table "pairs", force: :cascade do |t|
     t.date "day"
     t.string "pairs", default: [], array: true
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 
   create_table "profiles", force: :cascade do |t|
     t.string "first_name"
