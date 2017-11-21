2bec7a6250e41f0b9b11a2f6cc8ce18af5d2b23e
idearobin2017 <idearobin@gmail.com>
2017-11-21 12:10:13 +0100

    Update routes

--- app/models/pair.rb
+++ app/models/pair.rb
@@ -2,7 +2,7 @@ require "profile.rb"
 
 class Pair < ApplicationRecord
 
-	def make_pairs
+	def self.make_pairs
 		Profile.select_students_from_profiles
 		@pairs = RoundRobinTournament.schedule(@students)
 	end
