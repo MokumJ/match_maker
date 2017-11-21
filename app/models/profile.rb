fd94a24bed6d09782e68c05ecb86f9b0d00be07e
MokumJ <jesse.vanderheijden@gmail.com>
2017-11-21 11:05:51 +0100

    fix seeds

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,11 @@
 class Profile < ApplicationRecord
   belongs_to :user
+
+  def full_name
+"#{first_name} #{last_name}"
+end
+
+def self.select_students_from_profiles
+  @students = where(role: "student")
+end
 end
