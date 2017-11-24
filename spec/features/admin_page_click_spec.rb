444a70fe8d685afcbc4762c86e7be178ee9f6d39
MokumJ <jesse.vanderheijden@gmail.com>
2017-11-24 15:22:49 +0100

    fixing feature test

--- spec/features/admin_page_click_spec.rb
+++ spec/features/admin_page_click_spec.rb
@@ -1,26 +1,28 @@
 require 'rails_helper'
-  def log_in
+  def log_in(email, password)
     visit root_path
-    click_on 'Sign up'
-    fill_in 'Email', with: 'current@user.com'
-    fill_in 'Password', with: 'password'
+
+    fill_in 'Email', with: email
+    fill_in 'Password', with: password
     click_on 'Log in'
   end
 feature  "change to student" do
 
-  let(:user) { build :user, email: "current@user.com" , password: 'password'}
-  let(:another_user) { build :user, email: "another@user.com" }
-  let(:profile) { build :profile, first_name: "Robin " ,last_name: "Hood", role: "admin"}
-  let(:another_profile) { build :profile, first_name: "Jesse" ,last_name: "Jay", role: "student" }
 
-scenario "clicks on make_student" do
-  log_in
-  print page.html
-  #find('#navbar').click
-  #click_on 'User list'
-   #visit profiles_path
-   #print page.html
-  #   expect(page).to have_text( "Robin  Hood")
-    # expect(page).to have_text("user")
+
+
+scenario "clicks on make student" do
+
+  @user = FactoryGirl.create(:user, email: "jesja123@gmail.com", password: 'password')
+  profile = FactoryGirl.create(:profile, first_name: "Robin", last_name: "Hood", role: "admin", user: @user)
+visit root_path
+ log_in("jesja123@gmail.com", 'password')
+print page.html
+  find('#navbar').click
+  click_on 'User list'
+   visit profiles_path
+   print page.html
+     expect(page).to have_text( "Robin  Hood")
+     expect(page).to have_text("admin")
 end
 end
