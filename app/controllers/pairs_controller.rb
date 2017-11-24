bbe11c1b349644661a72050b5956713a687809a3
idearobin2017 <idearobin@gmail.com>
2017-11-24 14:00:24 +0100

    clean up codes

--- app/controllers/pairs_controller.rb
+++ app/controllers/pairs_controller.rb
@@ -1,5 +1,5 @@
 class PairsController < ApplicationController
-		before_action :set_students_array
+	before_action :set_students_array
 
 	def index
 		@pairs_schedule = Pair.all
@@ -8,17 +8,6 @@ class PairsController < ApplicationController
 	def create
 		save_into_db
 		redirect_to pairs_path
-	end
-
-	def save_into_db
-		Pair.all.where("day >= :today", today: Date.today ).destroy_all
-		set_students_array
-		@students = @students.shuffle
-
-		@schedule = Pairing.new(@students).days
-		@schedule.each do |day, pairs|
-			Pair.create!(day: Date.today + day, pairs: pairs)
-		end
 	end
 
 	private
@@ -38,4 +27,14 @@ class PairsController < ApplicationController
 		end
 	end
 
+	def save_into_db
+		Pair.all.where("day >= :today", today: Date.today ).destroy_all
+															# Destroy data that is from today
+		@students = @students.shuffle
+															# shuffle the array for randomizing the order
+		@schedule = Pairing.new(@students).days
+		@schedule.each do |day, pairs|
+			Pair.create!(day: Date.today + day, pairs: pairs)
+		end
+	end
 end
